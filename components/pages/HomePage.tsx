5bb876b66a5db9080294c97638c57432151d20a3
Dev Chaudhary <dev.chaudhary@industriometech.com>
2025-10-16 17:07:16 +0530

    reverted to without google verfication

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -6,9 +6,7 @@ import { ArrowRight, Plug, Database, Brain, Factory, Zap, Gauge, CheckCircle2 } 
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
 
-<head>
-  <meta name="google-site-verification" content="POLVP-3VSUzuFVQJ5HNJuF6oJ2J2eNdYWXmXoRz2uXU" />
-</head>
+
 
 
 interface HomePageProps {
